b4cd820a53a53a70983dcb9d083567240d715dd3
Mr-ADHD <mahbodnasourit@gmail.com>
2024-05-11 02:07:23 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+function App() {
+  return (
+    <div className="bg-red-600 h-6 w-6">
+      <p className="text-blue-50">
+        sldkfjslkdfj
+      </p>
+      
+      
+    </div>
+
+  );
+}
+
+export default App;
